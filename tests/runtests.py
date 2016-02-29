e6b829edd6ca86d446ff074cb6cfccfb04173f41
Ed <ed@xmos.com>
2016-02-29 07:22:08 +0000

    Modified runtests script to increase file limit on OSX hosts

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
-import xmostest
+import xmostest, sys, subprocess
 
 if __name__ == "__main__":
+    if sys.platform.startswith("darwin"):
+        p = subprocess.Popen("ulimit -n 1024", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in p.stdout.readlines():
+            print line,
+        retval = p.wait()
+        #OSX has a very low limit on open files - 256. Each spawn of xsim consumes about 5 so to run the 144 tests in this repo, we need at least 720 spare
     xmostest.init()
-    
-    xmostest.register_group("lib_src",
-                           "ssrc_tests",
-                           "Basic SSRC test 44.1KHz to 192KHz and 176.4KHz to 48KHz",
-                           "This test is run on the simulator using a 1KHz sine full scale input and compares the output with an expected set of data. The two tests (out of a possible 36) exercise six of the thirteen filters and so provide a basic smoke test of the datapath and FIR filter functions")
-    xmostest.register_group("lib_src",
-                            "asrc_tests",
-                            "Basic asrc test 44.1KHz to 192KHz and 176.4KHz to 48KHz using a 1:1 ratio",
-                            "This test is run on the simulator using a 1KHz sine full scale input and compares the output with an expected set of data. The two tests (out of a possible 36) exercise five of the twelve filters and so provide a basic smoke test of the datapath and FIR filter functions")
-    
     xmostest.runtests()
     xmostest.finish()
