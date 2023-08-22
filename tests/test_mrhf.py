931c902cad31531d13b4a119db413f3be572f1a6
Ed <ed@xmos.com>
2023-08-22 09:50:02 +0100

    Tidy and add tone_gen

--- tests/test_mrhf.py
+++ tests/test_mrhf.py
@@ -1,13 +1,12 @@
 # Copyright 2023 XMOS LIMITED.
-# This Software is subject to the terms of the XCORE VocalFusion Licence.
+# This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 """
-Test to check and explore the behaviour of the adaptive rate controller in usb_buffer.
-It uses a simplified model of usb_buffer with the actual sw_pll model and is a good representation of real world behaviour.
+Test verify the operation of both SSRC and ASRC (which belong to the multi-rate HiFi SRC collection)
 """
 
 import pytest
-from src_test_utils import prepare, build_firmware, build_host_app, compare_results, src_mrh_file_name_builder, run_dut
+from src_test_utils import gen_golden, build_firmware, build_host_app, compare_results, src_mrh_file_name_builder, run_dut
 
 
 NUM_SAMPLES_TO_PROCESS = 256
@@ -18,16 +17,17 @@ ASRC_DEVIATIONS = ("1.000000", "0.990099", "1.009999")
 @pytest.mark.prepare
 @pytest.mark.parametrize("src_type", ["ssrc", "asrc"])
 def test_prepare(src_type):
-    """ -- """
+    """ Builds firmware and host reference and generates the golden reference signals on the host """
     host_app = build_host_app(src_type)
     firmware = build_firmware(src_type)
-    prepare(host_app, firmware, src_type, NUM_SAMPLES_TO_PROCESS, ASRC_DEVIATIONS if src_type == "asrc" else None)
+    gen_golden(host_app, src_type, NUM_SAMPLES_TO_PROCESS, ASRC_DEVIATIONS if src_type == "asrc" else None)
   
 
 @pytest.mark.main
 @pytest.mark.parametrize("sr_out", SR_LIST)
 @pytest.mark.parametrize("sr_in", SR_LIST)
 def test_ssrc(sr_in, sr_out):
+    """ Runs the signal through the simulator via an xcore test app and compares to golden ref """
     run_dut(sr_in, sr_out, "ssrc", NUM_SAMPLES_TO_PROCESS)
 
 
@@ -36,4 +36,6 @@ def test_ssrc(sr_in, sr_out):
 @pytest.mark.parametrize("sr_in", SR_LIST)
 @pytest.mark.parametrize("fs_deviation", ASRC_DEVIATIONS)
 def test_asrc(sr_in, sr_out, fs_deviation):
+    """ Runs the signal through the simulator via an xcore test app and compares to golden ref """
+
     run_dut(sr_in, sr_out, "asrc", NUM_SAMPLES_TO_PROCESS, fs_deviation=fs_deviation)
