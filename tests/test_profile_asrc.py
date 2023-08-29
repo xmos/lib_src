30aa70844f95b09b38259e353c0127fd363afb04
Ed <ed@xmos.com>
2023-08-29 15:48:31 +0100

    Fix copyright

--- tests/test_profile_asrc.py
+++ tests/test_profile_asrc.py
@@ -1,5 +1,5 @@
 # Copyright 2023 XMOS LIMITED.
-# This Software is subject to the terms of the XCORE VocalFusion Licence.
+# This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 """
 Test to profile the execution time of ASRC
