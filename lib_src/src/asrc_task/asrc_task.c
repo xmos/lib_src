8db816595fb853e38e13277a52a7a5ab7c909861
Ed <ed@xmos.com>
2024-08-23 17:57:37 +0100

    ASRC task zeros invalid samples

--- lib_src/src/asrc_task/asrc_task.c
+++ lib_src/src/asrc_task/asrc_task.c
@@ -179,7 +179,10 @@ int par_asrc(int num_jobs, schedule_info_t schedule[], uint64_t fs_ratio, asrc_i
 int pull_samples(asrc_in_out_t * asrc_io, asynchronous_fifo_t * fifo, int32_t *samples, uint32_t output_frequency, int32_t consume_timestamp){
     asrc_io->output_frequency = output_frequency;
     if (asrc_io->ready_flag_configured){
-        asynchronous_fifo_consumer_get(fifo, samples, consume_timestamp);
+        // Zero samples if got samples are invalid
+        if(asynchronous_fifo_consumer_get(fifo, samples, consume_timestamp) == 0){
+            memset(samples, 0, fifo->channel_count * sizeof(samples[0]));
+        }
         return asrc_io->asrc_channel_count;
     }
     return 0;
