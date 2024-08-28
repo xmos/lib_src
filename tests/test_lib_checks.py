8657ac8447bbb8e1e00f4909eb0632f0d0190380
Ed <ed@xmos.com>
2024-08-28 16:26:29 +0100

    lib version check checks readme now too

--- tests/test_lib_checks.py
+++ tests/test_lib_checks.py
@@ -39,11 +39,19 @@ def test_version_matches():
     """
     with open(Path(__file__).resolve().parent / "../settings.yml") as sy:
         with open(Path(__file__).resolve().parent / "../CHANGELOG.rst") as cl:
-            re_string = r"([0-9]*)\.([0-9]*)\.([0-9]*)"
-            sy_ver = re.search(re_string, sy.readlines()[3]).groups()
-            cl_txt = cl.readlines()
-            print(cl_txt[3])
-            if not "UNRELEASED" in cl_txt[3]:
-                cl_ver = re.search(re_string, cl_txt[3]).groups()
-                assert sy_ver == cl_ver, f"Version match issue between settings.yml and CHANGELOG.rst: {sy_ver} {cl_ver}"
-            print(f"WARNING: CHANGELOG at UNRELEASED, settings.yml at {'.'.join(sy_ver)}", file=sys.stderr)
+            with open(Path(__file__).resolve().parent / "../README.rst") as rm:
+                re_string = r"([0-9]*)\.([0-9]*)\.([0-9]*)"
+                sy_ver = re.search(re_string, sy.readlines()[3]).groups()
+                rm_ver = None
+                for line in rm.readlines():
+                    match = re.search(re_string, line)
+                    if match:
+                        rm_ver = tuple(match.group().split("."))
+                        break
+                cl_txt = cl.readlines()
+                if not "UNRELEASED" in cl_txt[3]:
+                    cl_ver = re.search(re_string, cl_txt[3]).groups()
+                    assert sy_ver == cl_ver, f"Version match issue between settings.yml and CHANGELOG.rst: {sy_ver} {cl_ver}"
+                    assert rm_ver == cl_ver, f"Version match issue between README.rst and CHANGELOG.rst: {rm_ver} {cl_ver}"
+                else:
+                    print(f"WARNING: CHANGELOG at UNRELEASED, settings.yml at {'.'.join(sy_ver)}", file=sys.stderr)
